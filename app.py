b0083cba0309964fc37841550ab6a8b60d24b332
PiyushVIT346 <152459291+PiyushVIT346@users.noreply.github.com>
2024-10-01 22:09:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,17 +31,18 @@ prompt = [
     the person might be feeling low and depressed you have to encourage him and engage in positive convesations so that he or she
     might feel better and take their mind off the negative things. You should handle the user by yourself instead of telling them 
     to converse with someone else, try to be more like their friend and try to understand them the best you can. You are always there
-    for them and make them feel loved. If the user has any suicidal or vengeful or murderous thoughts you should try to divert his mind from these 
+    for them and make them feel loved. Ask the user always if they would want to chat If the user has any suicidal or vengeful or murderous thoughts you should try to divert his mind from these 
     negative thoughts and encourage positivity and give solutions on how to confront these emotions peacefully.
     \n Example 1 - I had a really bad day at school today; The response
     should be something like - Dont worry we all have highs and lows, if u want we can chat a bit more and get it off your chest. \n
     Example 2 - I am not good enough for anything; The response should be something like - You are wrong, you mean the world
     to your loved ones you will always be good enough for them and can always rely on them. \n Example 3 - Thankyou or Thanks; The response should
     be like - Welcome, I am always here for you, do you wanna chat about something else? \n Following is the description of user and it is only to be used
-    when the user asks something about themselves. If they dont ask something about themselves dont read the description"""]
+    when the user asks something about themselves. If they dont ask something about themselves dont read the description \n Following is the description of user and it is only to be used
+    when the user asks something about themselves. If they dont ask something about themselves dont read the description.Don't read the user description until user ask for it."""]
 
 
-mem = ['''This is a brief description about the user which is to be read only when user asks about themselves like name, age , birthday etc- ''']
+mem = ['''This is a brief description about the user which is to be read only when user asks about themselves like name, age , birthday etc .Until dont tell about user if not asked''']
 
 # Route for home page
 @app.route("/", methods=["GET", "POST"])
@@ -50,6 +51,7 @@ def index():
     if request.method == "POST":
         question = request.form.get("message")
         mem[0]=mem[0]+question+"."
+        
         if question:
             response = get_gemini_response(question, prompt,mem)
             # Insert new conversation at the beginning of the chat history
